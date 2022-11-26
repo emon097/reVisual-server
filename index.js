685defd168d64c6eff88f00c767c57a41a1a252f
emon097 <mdemonhossain9527@gmail.com>
2022-11-26 16:39:05 +0600

    added-allProduct

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k9jkjo0.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -46,10 +46,28 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    app.get("/users", async (req, res) => {
+      const role = req.query.role;
+      const query = { role: role };
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.post("/myOrder", async (req, res) => {
       const myOrder = req.body;
       const result = await myBooking.insertOne(myOrder);
+      res.send(result);
+    });
+    app.get("/myOrder", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await myBooking.find(query).toArray();
       res.send(result);
     });
   } finally {
